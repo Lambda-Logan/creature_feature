213a774ac051dd6c2facbddd60673c3229b6668b
Endrey Márk <ungazik@gmail.com>
2023-01-30 11:14:14 +0100

    Simple comparison group

--- benches/bench_criterionized_runner.rs
+++ benches/bench_criterionized_runner.rs
@@ -9,19 +9,23 @@ fn big_comparison_benchmark(c_manager: &mut Criterion) {
     }
     let bigstring = bigstring.as_str();
 
-    c_manager.bench_function(
+    let mut comparison_group = c_manager.benchmark_group("Comparison group");
+
+    comparison_group.bench_function(
         "benchmark_for_vec_of_2_length_array_slices()",
         |b_timer| b_timer.iter(
             || benchmark_for_vec_of_2_length_array_slices(black_box(bigstring))
         )
     );
 
-    c_manager.bench_function(
+    comparison_group.bench_function(
         "benchmark_for_vec_of_2_length_arrays()",
         |b_timer| b_timer.iter(
             || benchmark_for_vec_of_2_length_arrays(black_box(bigstring))
         )
     );
+
+    comparison_group.finish();
 }
 
 criterion_group!(benches, big_comparison_benchmark);
