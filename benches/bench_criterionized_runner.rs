11a430d466b143932cfd44ed32a05e9d6d554d01
Endrey Márk <ungazik@gmail.com>
2023-02-01 12:56:28 +0100

    Benchmark with comparison group, and with input, and also some boilerplate code is needed because of static parts

--- benches/bench_criterionized_runner.rs
+++ benches/bench_criterionized_runner.rs
@@ -1,5 +1,5 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use creature_feature::bench_criterionized::{benchmark_for_vec_of_2_length_array_slices, benchmark_for_vec_of_2_length_arrays};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
+use creature_feature::bench_criterionized::{benchmark_for_vec_of_n_length_array_slices, benchmark_for_vec_of_n_length_arrays};
 
 fn big_comparison_benchmark(c_manager: &mut Criterion) {
     let az = "abcdefghijklmnopqrstuvuxyz1$n34567890!@#$%^&*()";
@@ -11,19 +11,23 @@ fn big_comparison_benchmark(c_manager: &mut Criterion) {
 
     let mut comparison_group = c_manager.benchmark_group("Comparison group");
 
-    comparison_group.bench_function(
-        "benchmark_for_vec_of_2_length_array_slices()",
-        |b_timer| b_timer.iter(
-            || benchmark_for_vec_of_2_length_array_slices(black_box(bigstring))
-        )
-    );
+    for ref_n in [2, 4, 8, 16, 32, 64, 128].iter() {
+        comparison_group.bench_with_input(
+            BenchmarkId::new("benchmark_for_vec_of_n_length_array_slices", *ref_n),
+            ref_n,
+            |b_timer, ref_n| b_timer.iter(
+                || benchmark_for_vec_of_n_length_array_slices(black_box(bigstring), *ref_n)
+            )
+        );
 
-    comparison_group.bench_function(
-        "benchmark_for_vec_of_2_length_arrays()",
-        |b_timer| b_timer.iter(
-            || benchmark_for_vec_of_2_length_arrays(black_box(bigstring))
-        )
-    );
+        comparison_group.bench_with_input(
+            BenchmarkId::new("benchmark_for_vec_of_n_length_arrays", *ref_n),
+            ref_n,
+            |b_timer, ref_n| b_timer.iter(
+                || benchmark_for_vec_of_n_length_arrays(black_box(bigstring), *ref_n)
+            )
+        );
+    }
 
     comparison_group.finish();
 }
--- src/bench_criterionized.rs
+++ src/bench_criterionized.rs
@@ -1,14 +1,43 @@
-use crate::ftzrs::{bislice, bigram};
+use crate::ftzrs::{n_slice, n_gram};
 use crate::accum_ftzr::Ftzr; // because of the `featurize` method
 
 /// Benchmark #6: n_slice + Vec<&[u8]> (from &str)
-pub fn benchmark_for_vec_of_2_length_array_slices(bigstring: &str) -> usize {
-    let vec_of_2_length_array_slices: Vec<&[u8]> = bislice().featurize(bigstring);
-    vec_of_2_length_array_slices.len()
+pub fn benchmark_for_vec_of_n_length_array_slices(bigstring: &str, n: usize) -> usize {
+    let vec_of_n_length_array_slices: Vec<&[u8]> = n_slice(n).featurize(bigstring);
+    vec_of_n_length_array_slices.len()
 }
 
 /// Benchmark #7: n_gram + Vec<[u8; N]> (from &str)
-pub fn benchmark_for_vec_of_2_length_arrays(bigstring: &str) -> usize {
-    let vec_of_2_length_arrays: Vec<[u8; 2]> = bigram().featurize(bigstring);
-    vec_of_2_length_arrays.len()
+pub fn benchmark_for_vec_of_n_length_arrays(bigstring: &str, n: usize) -> usize {
+    match n {
+        2 => {
+            let vec_of_n_length_arrays: Vec<[u8;   2]> = n_gram::<2>  ().featurize(bigstring);
+            vec_of_n_length_arrays.len()
+        },
+        4 => {
+            let vec_of_n_length_arrays: Vec<[u8;   4]> = n_gram::<4>  ().featurize(bigstring);
+            vec_of_n_length_arrays.len()
+        },
+        8 => {
+            let vec_of_n_length_arrays: Vec<[u8;   8]> = n_gram::<8>  ().featurize(bigstring);
+            vec_of_n_length_arrays.len()
+        },
+        16 => {
+            let vec_of_n_length_arrays: Vec<[u8;  16]> = n_gram::<16> ().featurize(bigstring);
+            vec_of_n_length_arrays.len()
+        },
+        32 => {
+            let vec_of_n_length_arrays: Vec<[u8;  32]> = n_gram::<32> ().featurize(bigstring);
+            vec_of_n_length_arrays.len()
+        },
+        64 => {
+            let vec_of_n_length_arrays: Vec<[u8;  64]> = n_gram::<64> ().featurize(bigstring);
+            vec_of_n_length_arrays.len()
+        },
+        128 => {
+            let vec_of_n_length_arrays: Vec<[u8; 128]> = n_gram::<128>().featurize(bigstring);
+            vec_of_n_length_arrays.len()
+        }
+        _ => panic!("No matching")
+    }
 }
