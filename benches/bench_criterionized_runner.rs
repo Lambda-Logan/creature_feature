164b3f0bfb456f80ff3935a12714705aad659a3a
Endrey Márk <ungazik@gmail.com>
2023-02-06 13:33:00 +0100

    Add a 3rd, derivative comparison group to the 2 base groups: it represents a holistic cross-section sampling

--- benches/bench_criterionized_runner.rs
+++ benches/bench_criterionized_runner.rs
@@ -179,6 +179,101 @@ fn benchmarks_on_charvec_input(c_manager: &mut Criterion) {
     comparison_group_charvec_input.finish();
 }
 
+fn benchmarks_mixed_crosscutting_group(c_manager: &mut Criterion) {
+    let az = "abcdefghijklmnopqrstuvuxyz1$n34567890!@#$%^&*()";
+    let mut bigstring = "".to_owned();
+    for _ in 0..100 {
+        bigstring.push_str(&az);
+    }
+    let big_strslice = bigstring.as_str();
+    let big_charvec = chars_of(big_strslice);
 
-criterion_group!(benches, benchmarks_on_stringslice_input, benchmarks_on_charvec_input);
+    let mut comparison_group_mixed_crosscutting = c_manager.benchmark_group("Comparison group for mixed crosscutting");
+
+    // Note also the ampersand display problem: ASCII ampersand (&, i.e. U+0026) not appearing on plot diagram labels.
+    // Thus, at each `BechmarkId` label, I use the `small ampersand' ﹠(U+FE60) instead!
+    macro_rules! assemble_benchmarks_mixed_crosscutting {
+        ($n: expr) => {{
+            // The refactored benches out of the old benchmark module `bench.rs`:
+
+            // Input &str:
+
+            comparison_group_mixed_crosscutting.bench_with_input( // from old bench #1
+                BenchmarkId::new("n_slice + Vec<﹠str> (from ﹠str)", $n),
+                &$n,
+                |b_timer, ref_n| b_timer.iter(
+                    || {
+                        let vec_of_string_slices: Vec<&str> = n_slice(*ref_n).featurize(black_box(big_strslice));
+                        vec_of_string_slices.len()
+                    }
+                )
+            );
+            comparison_group_mixed_crosscutting.bench_with_input( // from old bench #6
+                BenchmarkId::new("n_slice + Vec<﹠[u8]> (from ﹠str)", $n), // note the ampersand
+                &$n,
+                |b_timer, ref_n| b_timer.iter(
+                    || {
+                        let vec_of_n_length_array_slices: Vec<&[u8]> = n_slice(*ref_n).featurize(black_box(big_strslice));
+                        vec_of_n_length_array_slices.len()
+                    }
+                )
+            );
+            comparison_group_mixed_crosscutting.bench_with_input( // from old bench #5
+                BenchmarkId::new("n_gram + Vec<HashedAs<u16>> (from ﹠str)", $n), // note the ampersand
+                &$n,
+                |b_timer, _| b_timer.iter(
+                    || {
+                        let vec_of_hashes: Vec<HashedAs<u16>> = n_gram::<$n>().featurize(black_box(big_strslice));
+                        vec_of_hashes.len()
+                    }
+                )
+            );
+            comparison_group_mixed_crosscutting.bench_with_input( // from old bench #4
+                BenchmarkId::new("n_slice + Vec<HashedAs<u16>> (from ﹠str)", $n), // note the ampersand
+                &$n,
+                |b_timer, ref_n| b_timer.iter(
+                    || {
+                        let vec_of_hashes: Vec<HashedAs<u16>> = n_slice(*ref_n).featurize(black_box(big_strslice));
+                        vec_of_hashes.len()
+                    }
+                )
+            );
+
+            // Input Vec<char>:
+
+            comparison_group_mixed_crosscutting.bench_with_input( // from old bench #10
+                BenchmarkId::new("n_gram + Vec<HashedAs<u64>> (from Vec<char>)", $n),
+                &$n,
+                |b_timer, _| b_timer.iter(
+                    || {
+                        let vec_of_hashes: Vec<HashedAs<u64>> = n_gram::<$n>().featurize(black_box(&big_charvec));
+                        vec_of_hashes.len()
+                    }
+                )
+            );
+            comparison_group_mixed_crosscutting.bench_with_input( // from old bench #9
+                BenchmarkId::new("n_slice + Vec<﹠[char]> (from Vec<char>)", $n), // note the ampersand
+                &$n,
+                |b_timer, ref_n| b_timer.iter(
+                    || {
+                        let vec_of_n_length_array_slices: Vec<&[char]> = n_slice(*ref_n).featurize(black_box(&big_charvec));
+                        vec_of_n_length_array_slices.len()
+                    }
+                )
+            );
+        }}
+    }
+
+    assemble_benchmarks_mixed_crosscutting!(  2);
+    assemble_benchmarks_mixed_crosscutting!(  4);
+    assemble_benchmarks_mixed_crosscutting!(  8);
+    assemble_benchmarks_mixed_crosscutting!( 16);
+    assemble_benchmarks_mixed_crosscutting!( 32);
+    assemble_benchmarks_mixed_crosscutting!( 64);
+    assemble_benchmarks_mixed_crosscutting!(128);
+
+    comparison_group_mixed_crosscutting.finish();
+}
+
+criterion_group!(benches, benchmarks_on_stringslice_input, benchmarks_on_charvec_input, benchmarks_mixed_crosscutting_group);
 criterion_main!(benches);
